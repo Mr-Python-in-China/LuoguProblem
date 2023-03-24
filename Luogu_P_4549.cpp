5ba0179b28e6c68a4a45b5cfc066977586b7efd2
Mr. Python <2789762371@qq.com>
2023-03-24 14:10:21 +0800

    modified:   .gitignore 	new file:   Luogu_P_1516.cpp 	new file:   Luogu_P_4549.cpp

---
+++ Luogu_P_4549.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ui=unsigned int;
+int main(void){
+    ios::sync_with_stdio(false),cin.tie(nullptr),cout.tie(nullptr);
+    size_t n;int ans;
+    cin>>n>>ans;
+    while (--n){
+        int a;
+        cin>>a;
+        a=abs(a);
+        ans=__gcd(a,ans);
+    }
+    cout<<ans;
+    return 0;
+}
